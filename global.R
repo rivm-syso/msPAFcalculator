bef9b0f4e8b011e3bcc4d0f57df69d6f793fc0b0
Twan Kranenburg <twan.kranenburg@rivm.nl>
2025-11-25 12:53:50 +0100

    config file

--- global.R
+++ global.R
@@ -1,15 +1,26 @@
 #global.R
 rm(list=ls())
 
+# Load config.yml
+# i: https://rshinydocs.rivm.nl/config.html
+# With this, we load in the directory
+if(!requireNamespace("config")) { install.packages("config") }
+if (file.exists("config.yml")) {
+  conf <- config::get()
+  gross_folder <- conf$data_directory
+} else {
+  # If you do not have the config-file, you can set the directory with this
+  gross_folder <- "/shiny-data/" #needed for shinyportaal.rivm.nl
+  #N.B. reset for local dev to:
+  #gross_folder <- "data"
+}
+
 
 #load in the Gross data
-#needed for shinyportaal.rivm.nl
-#gross_folder <- "/shiny-data/"
-#N.B. reset for local dev to:
-gross_folder <- "data/"
+
 
 #what are the gross files to load in
-SSDplusList <- readRDS(paste0(gross_folder,"SSDplusList.RDS"))
+SSDplusList <- readRDS(paste0(gross_folder,"/SSDplusList.RDS"))
 for (name in names(SSDplusList)) {
   #get the object name, based on the name
   assign(name, SSDplusList[[name]], envir = .GlobalEnv)
--- server.R
+++ server.R
@@ -37,7 +37,7 @@ server <- function(input, output, session) {
   output$Text_manual <- renderText({
     paste0(
       Textdata[Textdata$logical_name == "manual", input$languageMenu],
-      "\n App-version: ",getAppVersion(), # On a new line
+      "\n App version: ",getAppVersion(), # On a new line
       "\n git Head: ",git_head
     )
   })
