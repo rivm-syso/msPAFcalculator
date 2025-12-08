5363dff37792f70db56fb76f4607656379d927a6
Twan Kranenburg <twan.kranenburg@rivm.nl>
2025-12-08 15:59:06 +0100

    git head only in the warnings in the download file

--- server.R
+++ server.R
@@ -37,7 +37,7 @@ server <- function(input, output, session) {
   output$Text_manual <- renderText({
     paste0(
       Textdata[Textdata$logical_name == "manual", input$languageMenu],
-      "\n App version: ",getAppVersion(), # On a new line
+      "\n App version: ",getAppVersion()#, # On a new line
       #"\n git Head: ",git_head # Doesnt need to be in the app. Needs to be only in the warnings
     )
   })
